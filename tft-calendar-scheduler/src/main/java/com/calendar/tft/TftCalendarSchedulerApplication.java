2caec677d1eee86e791669f11faf845f136c48d6
seungtaek95 <seungtaek95@gmail.com>
2023-01-21 12:14:25 +0900

    scheduler 모듈 생성

---
+++ tft-calendar-scheduler/src/main/java/com/calendar/tft/TftCalendarSchedulerApplication.java
@@ -0,0 +1,11 @@
+package com.calendar.tft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TftCalendarSchedulerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TftCalendarSchedulerApplication.class, args);
+	}
+}
